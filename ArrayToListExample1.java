9ec380d6a94d7368b9ccdf3d6ade52d9a9baa772
Patel Chetan <110011555+chetanpmc@users.noreply.github.com>
2022-08-17 10:43:49 +0530

    Add files via upload

---
+++ ArrayToListExample1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class ArrayToListExample1
+{
+    public static void main(String args[])
+    {
+//creating an array and prints the array
+        String array[] = { "Cricket", "Baseball", "Volleyball",};
+        System.out.println("Array before conversion: "+ Arrays.toString(array));
+
+        //calling the generic function that converts Array into List And print the List
+        List<String> list = ArrayToListConversion(array);
+        System.out.println("Array as List: " + list);
+    }
+    //creating a generic function that converts the Array into List
+    public static <T> List<T> ArrayToListConversion(T array[])
+    {
+//creating the constructor of the List class
+        List<T> list = new ArrayList<>();
+//using for-each loop to iterate over the array
+        for (T t : array)
+        {
+//adding each element to the List
+            list.add(t);
+        }
+//returns the list converted into Array
+        return list;
+
+    }
+}
